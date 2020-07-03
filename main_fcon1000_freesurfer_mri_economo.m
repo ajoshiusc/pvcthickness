72bbc40d09e8ad32b8042c7ac94dc6ce5f0dc822
Anand A Joshi <ajoshi@masal.usc.edu>
2020-07-02 22:31:01 -0700

    updated and bug fix

--- main_fcon1000_freesurfer_mri_economo.m
+++ main_fcon1000_freesurfer_mri_economo.m
@@ -22,8 +22,8 @@ for j=1:length(l)
         unix('mris_ca_label -t ${DATA_DIR}/lh.colortable.txt ${SUBJECTNAME} lh ${SUBJECTS_DIR}/${SUBJECTNAME}/surf/lh.sphere.reg ${DATA_DIR}/lh.economo.gcs ${SUBJECTS_DIR}/${SUBJECTNAME}/label/lh.economo.annot');
         unix('mris_ca_label -t ${DATA_DIR}/rh.colortable.txt ${SUBJECTNAME} rh ${SUBJECTS_DIR}/${SUBJECTNAME}/surf/rh.sphere.reg ${DATA_DIR}/rh.economo.gcs ${SUBJECTS_DIR}/${SUBJECTNAME}/label/rh.economo.annot');
 
-        unix('mris_anatomical_stats -a ${SUBJECTNAME}/label/lh.economo.annot -f ${SUBJECTNAME}/stats/lh.economo.stats ${SUBJECTNAME} lh');
-        unix('mris_anatomical_stats -a ${SUBJECTNAME}/label/rh.economo.annot -f ${SUBJECTNAME}/stats/rh.economo.stats ${SUBJECTNAME} rh');
+        unix('mris_anatomical_stats -a ${SUBJECTS_DIR}/${SUBJECTNAME}/label/lh.economo.annot -f ${SUBJECTS_DIR}/${SUBJECTNAME}/stats/lh.economo.stats ${SUBJECTNAME} lh');
+        unix('mris_anatomical_stats -a ${SUBJECTS_DIR}/${SUBJECTNAME}/label/rh.economo.annot -f ${SUBJECTS_DIR}/${SUBJECTNAME}/stats/rh.economo.stats ${SUBJECTNAME} rh');
         
         
         fprintf('%d/%d subjects done\n',j,length(l));
