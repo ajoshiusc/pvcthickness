e5708c3f2f9875bd165566ac12fdc09dd3aa6468
Anand A. Joshi <ajoshi@usc.edu>
2025-05-18 02:06:00 -0700

    update

---
+++ HCP_multires_comparison/main_diff_2mm_orig.py
@@ -0,0 +1,148 @@
+# AUM
+# ||Shree Ganeshaya Namaha||
+
+import pandas as pd
+from tqdm import tqdm
+from time import sleep
+from shutil import copyfile
+from os import makedirs, system
+from os.path import join, isfile
+from multiprocessing import Pool
+from nilearn.image import resample_img
+import numpy as np
+from nilearn.masking import compute_background_mask
+import glob
+import os
+from dfsio import readdfs, writedfs
+
+
+def bstsvreg(subbasename):
+
+    # run brainsuite and svreg on the original image
+    if 0: #not isfile(subbasename + '.right.pial.cortex.dfs'):
+        # perform brainsuite and svreg processing
+        exe_name = "/home1/ajoshi/Projects/pvcthickness/cortical_extraction_nobse.sh"
+        if not os.path.isfile(exe_name):
+            exe_name = "/home/ajoshi/Projects/pvcthickness/cortical_extraction_nobse_local.sh"
+        
+        #system(f"{exe_name} {subbasename}")
+        system(f"sbatch /home1/ajoshi/Projects/pvcthickness/run_script.job \'{exe_name} {subbasename}\'")
+
+    if 0: #not isfile(subbasename + '.right.pial.cortex.svreg.dfs'):
+        # run svreg
+        exe_name = "/home1/ajoshi/Projects/SVRegSource000/compile_scripts/svreg_99_build0001_linux/bin/svreg.sh"
+        atlasbasename = "/home1/ajoshi/Projects/SVRegSource000/compile_scripts/svreg_99_build0001_linux/BCI-DNI_brain_atlas/BCI-DNI_brain"
+
+        if not os.path.isfile(exe_name):
+            exe_name = "/home/ajoshi/Software/BrainSuite21a/svreg/bin/svreg.sh"
+            atlasbasename = "/home/ajoshi/Software/BrainSuite21a/svreg/BCI-DNI_brain_atlas/BCI-DNI_brain"
+
+
+        #system(f"module load matlab/2024a; export BrainSuiteMCR=/apps/generic/matlab/2024a/; {exe_name} {subbasename}")
+        system(f"sbatch /home1/ajoshi/Projects/pvcthickness/run_script.job \'{exe_name} {subbasename} {atlasbasename} -S\'")
+
+    if isfile(subbasename + '.right.pial.cortex.svreg.dfs') and not isfile(subbasename + '.pvc-thickness_0-6mm.right.mid.cortex.dfs'):
+
+        # run thicknessPVC
+        exe_name = "/home1/ajoshi/Projects/SVRegSource000/compile_scripts/svreg_99_build0001_linux/bin/thicknessPVC.sh"
+        atlasbasename = "/home1/ajoshi/Projects/SVRegSource000/compile_scripts/svreg_99_build0001_linux/BCI-DNI_brain_atlas/BCI-DNI_brain"
+
+        if not os.path.isfile(exe_name):
+            exe_name = "/home/ajoshi/Software/BrainSuite21a/svreg/bin/thicknessPVC.sh"
+            atlasbasename = "/home/ajoshi/Software/BrainSuite21a/svreg/BCI-DNI_brain_atlas/BCI-DNI_brain"
+
+
+        #system(f"module load matlab/2024a; export BrainSuiteMCR=/apps/generic/matlab/2024a/; {exe_name} {subbasename}")
+        system(f"sbatch /home1/ajoshi/Projects/pvcthickness/run_script.job \'{exe_name} {subbasename}\'")
+
+    if isfile(subbasename + '.pvc-thickness_0-6mm.right.mid.cortex.dfs') and not isfile(subbasename[-2] + 'atlas.pvc-thickness_0-6mm.right.mid.cortex.dfs'):
+
+        # run svreg_thickness2atlas
+        exe_name = "/home1/ajoshi/Projects/SVRegSource000/compile_scripts/svreg_99_build0001_linux/bin/svreg_thickness2atlas.sh"
+        atlasbasename = "/home1/ajoshi/Projects/SVRegSource000/compile_scripts/svreg_99_build0001_linux/BCI-DNI_brain_atlas/BCI-DNI_brain"
+
+        if not os.path.isfile(exe_name):
+            exe_name = "/home/ajoshi/Software/BrainSuite21a/svreg/bin/svreg_thickness2atlas.sh"
+            atlasbasename = "/home/ajoshi/Software/BrainSuite21a/svreg/BCI-DNI_brain_atlas/BCI-DNI_brain"
+
+
+        #system(f"module load matlab/2024a; export BrainSuiteMCR=/apps/generic/matlab/2024a/; {exe_name} {subbasename}")
+        system(f"sbatch /home1/ajoshi/Projects/pvcthickness/run_script.job \'{exe_name} {subbasename}\'")
+    
+
+
+def main():
+
+    hcp_proj11183 = "/project/ajoshi_1183"
+    outdir = join("/project/ajoshi_1183/HCP_data_multires")
+
+    # get a list of subjects
+    sub_list = glob.glob(
+        f"{hcp_proj11183}/volume1/masal/big_disk/ajoshi/ajoshi/HCP100_struct/*"
+    )
+
+    hcpsubs = [os.path.basename(x) for x in sub_list]
+
+    if not os.path.isdir(hcp_proj11183):
+        # this means I am running on my local machine
+        # so only process 2 subjects
+        hcp_proj11183 = "/home/ajoshi/project_ajoshi_1183"
+        outdir = join("/home/ajoshi/HCP_data_multires")
+        sub_list = glob.glob(
+            f"{hcp_proj11183}/volume1/masal/big_disk/ajoshi/ajoshi/HCP100_struct/*"
+        )
+        hcpsubs = [os.path.basename(x) for x in sub_list]
+        hcpsubs = hcpsubs[:2]
+
+    # /*/T1w_acpc_dc_restore_brain.nii.gz
+
+    diff_left_thickness_all = []
+    diff_right_thickness_all = []
+    # make output dirs for 4 different resolutions, orig, 1mm, 2mm, 3mm
+    for sub in tqdm(hcpsubs):
+        outdir_sub = join(outdir, sub)
+
+        hires_left_file = join(outdir_sub, "orig", "atlas.pvc_thickness.left.svreg.dfs")
+        lowres_left_file = join(outdir_sub, "orig", "atlas.pvc_thickness.left.svreg.dfs")
+        hires_right_file = join(outdir_sub, "orig", "atlas.pvc_thickness.right.svreg.dfs")
+        lowres_right_file = join(outdir_sub, "orig", "atlas.pvc_thickness.right.svreg.dfs")
+
+
+        hires_left_thickness = readdfs(hires_left_file)
+        lowres_left_thickness = readdfs(lowres_left_file)
+        hires_right_thickness = readdfs(hires_right_file)
+        lowres_right_thickness = readdfs(lowres_right_file)
+        diff_left_thickness = hires_left_thickness - lowres_left_thickness
+        diff_right_thickness = hires_right_thickness - lowres_right_thickness
+        diff_left_thickness = np.abs(diff_left_thickness)
+        diff_right_thickness = np.abs(diff_right_thickness)
+        diff_left_thickness_all.append(diff_left_thickness)
+        diff_right_thickness_all.append(diff_right_thickness)
+    
+
+    # take average of the differences over all subjects and save as dfs
+    diff_left_thickness_all = np.array(diff_left_thickness_all)
+    diff_right_thickness_all = np.array(diff_right_thickness_all)
+    diff_left_thickness_all = np.mean(diff_left_thickness_all, axis=0)
+    diff_right_thickness_all = np.mean(diff_right_thickness_all, axis=0)
+
+    # save as dfs
+    writedfs(
+        diff_left_thickness_all,
+        join(outdir, "diff_left_thickness.dfs"),
+        sub_list[0],
+    )
+    writedfs(
+        diff_right_thickness_all,
+        join(outdir, "diff_right_thickness.dfs"),
+        sub_list[0],
+    )
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ HCP_multires_comparison/main_thickness_diff_hires_vs_2mm.m
@@ -0,0 +1,37 @@
+clc;clear ;close all;
+restoredefaultpath;
+addpath(genpath('/home/ajoshi/projects/BrainSuite/svreg/dev'));
+addpath(genpath('/home/ajoshi/projects/BrainSuite/svreg/src'));
+
+hires='/home/ajoshi/projects/pvcthickness/hires_lowres_comparison/hires_data'
+lowres='/home/ajoshi/projects/pvcthickness/hires_lowres_comparison/lowres_data'
+
+sl=readdfs([hires,'/atlas.pvc-thickness_0-6mm.right.mid.cortex.dfs']);
+%smooth_surf_function(sl,sl.attributes);
+thickness_1mm = min(max(sl.attributes,0),6);
+
+sl=readdfs([lowres,'/atlas.pvc-thickness_0-6mm.right.mid.cortex.dfs']);
+%smooth_surf_function(sl,sl.attributes);
+thickness_3mm = min(max(sl.attributes,0),6);
+
+
+sl.attributes = abs(thickness_3mm - thickness_1mm)
+sl.attributes=smooth_surf_function(sl,sl.attributes);
+
+sl.attributes = 0.75*sl.attributes/5;
+
+
+h=figure;
+
+
+patch('vertices',sl.vertices,'faces',sl.faces,'facevertexcdata',sl.attributes,'facecolor','interp','edgecolor','none');
+axis equal;axis off;camlight;axis tight;
+caxis([0,.75]);colormap jet;material dull;
+view(-90,0);camlight('headlight'); 
+saveas(h,'ADE_right_hireslowres_1.png')
+view(90,0);camlight('headlight'); 
+saveas(h,'ADE_right_hireslowres_2.png')
+close all;
+
+save hires_lowres_right_pvc
+
